437b6eb095af40e6600cbebaba41c0429b3c1571
taliafabs <talia.fabregas@mail.utoronto.ca>
2022-10-08 22:42:57 -0400

    intro to java coding exercise 1

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -1,13 +1,4 @@
 /**
- * This file contains a few exercises to familiarize you with Java.
- * You should read this file from top-to-bottom. Any tasks you are to complete
- * are labelled with TODO
- *
- * For your convenience, we have also included references to the
- * relevant readings for each task.
- */
-
-/**
  *  1. In Java, no code can exist outside of a class. Unlike Python which uses
  *    functions, all code in Java uses methods.
  *
@@ -25,20 +16,13 @@ public class Basics {
      *        public static void main(final String[] args){
      *            // Code goes in here
      *        }
-     *
      *    When you run this file (Basics.java), the code within the main
      *    method is run.
-     *
      *    (Relevant reading: 1.1.2. Defining methods)
      */
     public static void main(final String[] args) {
         System.out.println(7 + 5);
-
-        /* TODO (Task 1): Write a line of code below that prints the string
-         *                Hello World!
-         */
         System.out.println("Hello World!");
-
         /* 4. In Python, we could declare variables using a simple assignment
          *    statement. For example:
          *        x = 10
@@ -49,11 +33,6 @@ public class Basics {
          *        int x;
          *
          *    (Relevant reading: 1.2. Variables and Types)
-         */
-
-        /*
-         * TODO (Task 2): Create a variable named my_variable and assign it the
-         *                value 100.
          */
          int my_variable = 100;
 
@@ -80,10 +59,6 @@ public class Basics {
          */
 
         /*
-         * TODO (Task 3): Create a for-loop that loops from 10 to 0, printing
-         *                the following at each iteration:
-         *                Current count: #
-         *
          * e.g. the end result should look like:
          * Current count: 10
          * Current count: 9
@@ -126,14 +101,6 @@ public class Basics {
      * @return           The first letter of every word in to_split
      */
     public static String split(String to_split) {
-        /* TODO (Task 4): Complete this method body.
-         *                The String methods .split and .charAt may be helpful,
-         *                along with the StringBuilder.append
-         *                You may also assume that to_split always has 7 words
-         *                separated by spaces, so no loops are required, though
-         *                you may use them if you wish.
-         *
-         */
         StringBuilder ret = new StringBuilder();
         String[] to_split_array = to_split.split(" ");
 
@@ -163,31 +130,12 @@ public class Basics {
             return 0;
         }
         else {
-
-
-            /* TODO (Task 5): Complete this method body using a for-loop.
-             *                You can find the length of an array by using the
-             *                .length attribute (e.g. arr.length)
-             *                You can index into arrays as we do in Python
-             *                (e.g. arr[i] gives you the item at index i).
-             */
             for (int index = 1; index < arr.length; index = index + 2)
             {
                 current_sum += arr[index];
             }
-
             return current_sum;
         }
     }
 
-
-    /*
-     * TODO (Task 6): Submit the changes you made on GitHub!
-     *                When you submit it, go to the 'Actions' tab. You should
-     *                be able to see this exercise being autograded!
-     *
-     *                You can click on the ongoing action to see more details.
-     *                If the tests don't pass, look at the results and revise
-     *                accordingly.
-     */
 }
